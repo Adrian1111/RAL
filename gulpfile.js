e00723bc89dd06c6c6439ce2c972e0fa9bce0ba5
Adrian1111 <adrian111111a@gmail.com>
2017-05-01 21:18:48 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 var gulp = require('gulp');
 var shorthand = require('gulp-shorthand');
- 
-gulp.task('default', function () {
+
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+
+    console.log('Test');
+
+});
+
+gulp.task('css-short', function () {
     return gulp.src('src/styles.css')
         .pipe(shorthand())
         .pipe(gulp.dest('dest'));
+});
+
+var moreCSS = require('gulp-more-css');
+ 
+gulp.task('css-more', function() {
+    return gulp.src('src/styles.css')
+        .pipe(moreCSS())
+        .pipe(gulp.dest('./out'));
 });
