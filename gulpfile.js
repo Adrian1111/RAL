fc3d1eac1d92bda385f3b3112dcc28aab07eef4e
Adrian1111 <adrian111111a@gmail.com>
2017-05-01 21:27:58 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -22,3 +22,22 @@ gulp.task('css-more', function() {
         .pipe(moreCSS())
         .pipe(gulp.dest('./out'));
 });
+
+var csslint = require('gulp-csslint');
+
+gulp.task('css-lint', function() {
+    gulp.src('src/styles.css')
+        .pipe(csslint())
+        .pipe(csslint.formatter());
+});
+
+var gulp = require('gulp');
+var uncss = require('gulp-uncss');
+
+gulp.task('unused', function () {
+    return gulp.src('src/styles.css')
+        .pipe(uncss({
+            html: ['src/index.html']
+        }))
+        .pipe(gulp.dest('./out'));
+});
