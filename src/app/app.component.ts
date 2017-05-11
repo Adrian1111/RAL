41ce7e953abcb048b53fed8927fc6c0e8a48cb80
adrian <adrian111111a@gmail.com>
2017-05-11 08:28:21 +0200

    template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `
-    <h1 class="title">Reddit Articles</h1>
-    <router-outlet></router-outlet>
-    <router-outlet name="popup"></router-outlet>
-  `
+  templateUrl: '/app/app.component.html'
 })
 export class AppComponent {
 }
