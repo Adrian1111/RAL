9b2f6c743cbfa1ba6db54d2c54a472949daa91fd
adrian <adrian111111a@gmail.com>
2017-05-11 08:32:42 +0200

    template

--- src/app/articles/article-detail.component.ts
+++ src/app/articles/article-detail.component.ts
@@ -7,47 +7,7 @@ import { Observable } from 'rxjs/Observable';
 import { DomSanitizer  } from '@angular/platform-browser';
 
 @Component({
-  template: `
-<div *ngIf="article">
-    <h2>"{{ article.title }}"</h2>
-      <iframe width="1200" height="700" [src]="sanitizer.bypassSecurityTrustResourceUrl(article.url)"></iframe>
-  </div>
-  <div class="container">
-  <h2>Comments</h2>
-  <ul>
-      <li *ngFor="let comment of comments | async">
-        <span class="badge">{{ comment.author }}</span> {{ comment.body }}
-      </li>
-   </ul>
-</div>
-   
-<div class="container">
-  <h2>Add a comment</h2>
-  <div *ngIf="commentSent" class="alert alert-success alert-dismissable fade in">
-    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-    <strong>Success!</strong> Comment has been added.
-  </div>
-  <form class="form-horizontal">
-    <div class="form-group">
-      <label class="control-label col-sm-2" for="email">Your comment:</label>
-      <div class="col-sm-10">
-        <input [(ngModel)]="textComment" type="text" class="form-control"
-         id="comment" placeholder="Enter text" name="comment">
-      </div>
-    </div>
-    <div class="form-group">        
-      <div class="col-sm-offset-2 col-sm-10">
-        <button (click)="commentSent = true; postComment('json', textComment, article.id)"
-         type="submit" class="btn btn-default" [disabled]="textComment == null">Submit</button>
-      </div>
-    </div>
-  </form>
-</div>
-
-   <p class="bottomBtnsB">
-      <button (click)="gotoArticles()">Back</button>
-   </p>
-  `
+  templateUrl: './article-detail.component.html'
 })
 export class ArticleDetailComponent implements OnInit {
   @HostBinding('style.display')   display = 'block';
